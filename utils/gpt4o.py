faa7c5db7ba3a547a26f1baac6e07c1fe4307b36
Stavr Moris <stavrmoris@yandex.com>
2025-03-29 12:31:09 +0700

    Update gpt4o.py

--- utils/gpt4o.py
+++ utils/gpt4o.py
@@ -2,10 +2,7 @@ import requests
 import utils.database as db
 from openai import OpenAI
 
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key="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",
-)
+url = 'https://testsites.pythonanywhere.com/gpt'
 
 async def text_generate(user_id, prompt, model):
     print(prompt)
@@ -20,23 +17,23 @@ async def text_generate(user_id, prompt, model):
         tokens = db.get_variable(user_id, 'gpt4omini-usage')
         db.set_variable(user_id, 'gpt4omini-usage', int(tokens)-1)
 
-    completion = client.chat.completions.create(
-        extra_headers={},
-        extra_body={},
-        model=model,
-        messages=[
+    data = {
+        "data": [
             {
                 "role": "user",
                 "content": [
                     {
                         "type": "text",
-                        "text": f"{history}"
+                        "text": history
                     }
                 ]
             }
-        ]
-    )
-    answer = completion.choices[0].message.content
+        ],
+        "model": model
+    }
+
+    response = requests.post(url, json=data, verify=False)
+    answer = response.text
     print(answer)
     db.add_message(user_id, f'Ты написал: {answer}')
     return answer
