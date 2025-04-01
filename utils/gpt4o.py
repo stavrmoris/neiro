f7e33a190c1754dff3b403bc3d3a07739c3d8ef5
Stavr Moris <stavrmoris@yandex.com>
2025-04-01 20:20:35 +0700

    t8

--- utils/gpt4o.py
+++ utils/gpt4o.py
@@ -2,14 +2,17 @@ import requests
 import utils.database as db
 from openai import OpenAI
 
-url = 'https://testsites.pythonanywhere.com/gpt'
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key="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",
+)
 
 async def text_generate(user_id, prompt, model):
     print(prompt)
     db.add_message(user_id, f'Я написал: {prompt}')
     history = db.get_message_history(user_id)
     if model == 'gpt-4o':
-        model = "google/gemini-2.5-pro-exp-03-25:free"
+        model = "deepseek/deepseek-r1-zero:free"
         tokens = db.get_variable(user_id, 'gpt4o-usage')
         db.set_variable(user_id, 'gpt4o-usage', int(tokens)-1)
     else:
@@ -17,23 +20,18 @@ async def text_generate(user_id, prompt, model):
         tokens = db.get_variable(user_id, 'gpt4omini-usage')
         db.set_variable(user_id, 'gpt4omini-usage', int(tokens)-1)
 
-    data = {
-        "data": [
+    completion = client.chat.completions.create(
+        extra_headers={},
+        extra_body={},
+        model=model,
+        messages=[
             {
                 "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": f"{history}"
-                    }
-                ]
+                "content": f"{history}"
             }
-        ],
-        "model": model
-    }
-
-    response = requests.post(url, json=data, verify=False)
-    answer = response.text
+        ]
+    )
+    answer = completion.choices[0].message.content
     print(answer)
     db.add_message(user_id, f'Ты написал: {answer}')
     return answer
